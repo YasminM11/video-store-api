e1bd57f01509ac1d9c848e26368b4ab36077fbe3
Yasmin <yasmin312002@hotmail.com>
2019-11-05 17:16:50 -0800

    added index to the customers_controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,15 @@
+JSON_KEYS = [:id, :name, :register_at, :address, :city, :state, :postal_code, :phone]
+
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all.as_json(only: JSON_KEYS)
+    render json: customers, status: :ok
+  end
+
+
+    private
+
+  def customer_params
+    params.require(:customer).permit(:id, :name, :register_at, :address, :city, :state, :postal_code, :phone)
+  end
 end
