99b94db355264a06792e7fb6c1dd928f301abfce
Yasmin <yasmin312002@hotmail.com>
2019-11-06 14:15:29 -0800

    added customers.yml file, and added tests to customers_controlle, and added customer route to routes file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,28 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'minitest/rails'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
+
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def check_response(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header["Content-Type"]).must_include "json"
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
 end
