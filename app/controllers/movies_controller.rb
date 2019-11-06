f3fcab74cd9433dbe8fdaf5e16772b6587b7d084
Yasmin <yasmin312002@hotmail.com>
2019-11-05 16:20:36 -0800

    added index, and show methods to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,28 @@
+KEYS = [:id, :title, :overview, :relase_date, :inventory]
+
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all.as_json(only: KEYS)
+    render json: movies, status: :ok
+  end
+
+  def show
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    if movie
+      render json: movie.as_json(only: KEYS) #status: :ok
+      return
+    else
+      render json: movie, status: :not_found
+      #render json: {"errors"=>["not found"]}, status: :not_found
+      return
+    end
+
+    
+  end
+
+  private
+    def pet_params
+      params.require(:movie).permit(:id, :title, :overview, :relase_date, :inventory)
+    end
 end
