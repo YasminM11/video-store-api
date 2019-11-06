b2987629cf127d506a7d816a308ed52bc9cc4c3c
Erika <erika.maust@gmail.com>
2019-11-06 08:19:25 -0800

    Movie controller index action test written (untested)

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+ 
+  describe index do
+
+    it "respoonds with json and responds with success" do
+      gets movies_path
+      expect(response.header["Content-Type"]).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it "provides the expected array of movies" do
+      get movies_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of array
+      body.each do |movie|
+        expect(movie).must_be_instance_of Hash
+        expect(movie.keys.sort).must_equal["title", "overview", "release_date", "inventory"]
+      end
+    end
+
+    it "responds with empty array when there are no movies" do
+      Movies.destroy_all
+
+      get movies_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of array
+      expect(body).must_equal []
+    end
+  end
+
 end
