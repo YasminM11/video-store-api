5d69e04c884bde53e4ac2054797657ca6a7070a5
Erika <erika.maust@gmail.com>
2019-11-06 08:36:10 -0800

    Rental controller index, show, and create actions written

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,32 @@
 class RentalsController < ApplicationController
+
+    def index
+        @rentals = Rental.all
+        rental = Rental.all.as_json(only: [:customer_id, :movie_id, :checkout, :due_date])
+        render json: rental, status: :ok
+    end
+
+    def show
+        rental = Rental.find_by(id: params:[:id])
+
+        if rental
+            render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory])
+            return
+        else
+            render json: { ok: false, errors: ["Not Found"] }, status: :not_found
+            return
+        end
+    end
+
+    def create
+        rental = Rental.new(rental_params)
+
+        if rental.save
+            render json: rental.as_json(only [:id]), status: :created
+            return
+        else
+            render json: {ok: false, errors: rental.errors.messages }, status: :bad_request
+            return
+        end
+    end
 end
