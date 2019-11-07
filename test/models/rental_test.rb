715f572f5ed5d5414ba1e1df8c1cf2c791da3887
Erika <erika.maust@gmail.com>
2019-11-07 09:17:23 -0800

    Started writing model validations for rental

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "validations" do
+    describe "customer id validation" do
+      it "is invalid without a customer id" do
+        customer_id = customer_id(:customer_id)
+        rental.customer_id = nil
+
+        result = rental.valid?
+        expect(result).must_equal false
+      end
+
+      it "is valid with a customer id" do
+       customer_id = customer_id(:customer_id)
+       rental.customer_id = 2
+
+        # Act
+        result = rental.valid?
+        expect(result).must_equal true
+      end
+    end
+
 end
