e6433dee35b05881f58970af6d8bd512f5ebc63d
Yasmin <yasmin312002@hotmail.com>
2019-11-07 10:07:56 -0800

    added tests to model customer not passing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   
+  has_many :rental
+
+# validations
+validates :name, presence: true, uniqueness: true
 
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,24 +1,40 @@
 require "test_helper"
 
-# let (:erika) { customers(:customer1) }
-
 describe Customer do
+  let (:erika) { customers(:customer1) }
 
-# it "requires id, name, address, city, state, postal_code, phone" do
-#   required_fields = [:id, :name, :address, :city, :state, :postal_code, :phone]
+  it "require a name" do 
+    erika[:name] = nil
+    expect(erika.valid?).must_equal false
+  end
+  describe 'relations' do  
+    
+      it 'has many rentals' do
+      # customer = customers(:customer1)
+      erika.must_respond_to :rental
+      expect(customer.rentals).must_equal 2
+      erika.rentals.each do |rental|
+      expect(rental).must_be_instance_of Rental
+      end
+      it 'has no rentals' do
+      erika.rentals.destroy_all
+      erika.must_respond_to :rental
+      expect(rental).must_equal nil
+    end
 
-#   required_fields.each do |field|
-#     erika[field] = nil
+  end
+end
+    
+    
 
-#     expect(erika.valid?).must_equal false
+  
 
-#     erika.reload
-#   end
-# end
 
-# it "requires an address" do
-#   erika.address = "546 435th pl sw"
+  # it "requires valid information for the customer" do
+  #   required_fields = [:name, :address, :city, :state, :postal_code, :phone]
+  #     expect(erika.name).must_equal "erika"
+  # end
+  # it "return an error if the info is not valid"
+  # required_fields = [:name, :address, :city, :state, :postal_code, :phone]
 
-#   expect(erika.valid?).must_equal false
-# end
 end
