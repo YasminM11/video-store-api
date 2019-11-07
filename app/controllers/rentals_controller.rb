7956ba308bbe2beeb945bfe1aebbd2c15561c6f3
Erika <erika.maust@gmail.com>
2019-11-06 23:37:37 -0800

    Wrote tests for movies controller, refactored movies controller as needed. Created movies yml for testing, and added necessary items to test helper

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,38 @@
 class RentalsController < ApplicationController
+
+  def index
+   @rentals = Rental.all
+    rental = Rental.all.as_json(only: [:customer_id, :movie_id, :checkout, :due_date])
+    render json: rental, status: :ok
+  end
+
+  # def show
+  #   rental = Rental.find_by(id: params:[:id])
+  #     if rental
+  #       render json: rental.as_json(only: [:id, :customer_id, :movie_id, :checkout, :due_date])
+  #       return
+  #     else
+  #       render json: { ok: false, errors: ["Not Found"] }, status: :not_found
+  #       return
+  #     end
+  # end
+
+  # def create
+  #   rental = Rental.new(rental_params)
+  #   if rental.save
+  #     render json: rental.as_json(only: [:id]), status: :created
+  #     return
+  #   else
+  #     render json: {ok: false, errors: rental.errors.messages }, status: :bad_request
+  #     return
+  #   end
+  # end
+
+
+  # private
+  #   def rental_params
+  #     params.require(:rental).permit(:id, :customer_id, :movie_id, :checkout, :due_date)
+  #   end
+
+
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,11 +1,14 @@
 require "test_helper"
 
+JSON_KEYS = ["id", "title", "overview", "release_date", "inventory"]
+
 describe MoviesController do
  
-  describe index do
+  describe 'index' do
 
-    it "responds with json and responds with success" do
-      gets movies_path
+    it "responds with JSON and responds with success" do
+      get movies_path
+
       expect(response.header["Content-Type"]).must_include 'json'
       must_respond_with :ok
     end
@@ -14,22 +17,47 @@ describe MoviesController do
       get movies_path
       body = JSON.parse(response.body)
 
-      expect(body).must_be_instance_of array
+      expect(body).must_be_instance_of Array
       body.each do |movie|
         expect(movie).must_be_instance_of Hash
-        expect(movie.keys.sort).must_equal["title", "overview", "release_date", "inventory"]
+        expect(movie.keys.sort).must_equal ["id", "title", "overview", "release_date", "inventory"].sort
       end
     end
 
     it "responds with empty array when there are no movies" do
-      Movies.destroy_all
+      Movie.destroy_all
 
       get movies_path
       body = JSON.parse(response.body)
 
-      expect(body).must_be_instance_of array
+      expect(body).must_be_instance_of Array
       expect(body).must_equal []
     end
   end
 
+  
+  describe "show" do
+    
+    it "accurately retrieves the information for a movie" do
+      movie = movies(:movie1)
+
+      get movie_path(movie.id)
+      expect(movie).must_be_instance_of Movie
+    end
+
+   
+    it "returns not found if the movie does not exist" do
+      
+      get movie_path(-1)    
+      must_respond_with :not_found
+  
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+      expect(body.keys).must_include "errors"    
+  end
+
+  end
+
+
 end
+
+
