3140248ed8ab8aa5bdd367c0cc803ba82dd102d7
Yasmin <yasmin312002@hotmail.com>
2019-11-07 16:18:46 -0800

    added validation to all the parts in the customer model test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,11 @@ class Customer < ApplicationRecord
 
 # validations
 validates :name, presence: true, uniqueness: true
+validates :address, presence: true, uniqueness: true
+validates :city, presence: true, uniqueness: true
+validates :state, presence: true, uniqueness: true
+validates :postal_code, presence: true, uniqueness: true
+validates :phone, presence: true, uniqueness: true
+
 
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,25 +1,87 @@
 require "test_helper"
 
 describe Customer do
-  let (:erika) { customers(:customer1) }
+  let (:valid_customer) { customers(:customer1) }
 
-  describe 'validation' do  
-    it "require a name" do 
-      erika[:name] = nil
-      expect(erika.valid?).must_equal false
+  describe 'validation' do
+    describe 'validate customer name' do  
+      it "is invalid without a name" do 
+        valid_customer[:name] = nil
+        expect(valid_customer.valid?).must_equal false
+      end
+
+      it "works if the name is valid" do
+        expect(valid_customer.valid?).must_equal true
+      end
+    end
+ 
+
+    describe 'validate address' do
+      it "is invalid without an address" do
+        valid_customer[:address] = nil
+        expect(valid_customer.valid?).must_equal false
+      end
+
+
+      it "works if the address is valid" do
+        expect(valid_customer.valid?).must_equal true
+      end
     end
 
-    it "works  if the name is valid" do
-      expect(erika.valid?).must_equal true
+    describe 'validate city' do
+      it "is invalid without a city" do
+        valid_customer[:city] = nil
+        expect(valid_customer.valid?).must_equal false
+      end
+      
+
+      it "works if the city is valid" do
+        expect(valid_customer.valid?).must_equal true
+      end
+    end
+
+    describe 'validate state' do
+      it "is invalid without a state" do
+          valid_customer[:state] = nil
+          expect(valid_customer.valid?).must_equal false
+        end
+        
+      it "works if the state is valid" do
+        expect(valid_customer.valid?).must_equal true
+      end
+    end
+
+    describe 'validate postal_code' do
+      it "is invalid without a postal_code" do
+            valid_customer[:postal_code] = nil
+            expect(valid_customer.valid?).must_equal false
+          end
+        
+      it "works if the postal_code is valid" do
+        expect(valid_customer.valid?).must_equal true
+      end
+    end
+
+    describe 'validate phone' do
+      it "is invalid without a phone" do
+            valid_customer[:phone] = nil
+            expect(valid_customer.valid?).must_equal false
+          end
+        
+      it "works if the phone is valid" do
+        expect(valid_customer.valid?).must_equal true
+      end
     end
   end
+
+
 
   describe 'relations' do  
       
     it "has many rentals" do
-      erika.must_respond_to :rentals
-      # expect(erika.rentals.length).must_equal 2
-      erika.rentals.each do |rental|
+      valid_customer.must_respond_to :rentals
+      # expect(valid_customer.rentals.length).must_equal 2
+      valid_customer.rentals.each do |rental|
         expect(rental).must_be_kind_of Rental
       end
     end
