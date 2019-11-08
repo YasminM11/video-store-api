884c1535afa64fc641324582c6318ff61a11863b
Erika <erika.maust@gmail.com>
2019-11-07 19:36:49 -0800

    Removed unnecessary attributes from JSON_KEYS in customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,16 @@
-JSON_KEYS = [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count]
+JSON_KEYS = [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]
 
 class CustomersController < ApplicationController
+  
   def index
     customers = Customer.all.as_json(only: JSON_KEYS)
     render json: customers, status: :ok
   end
 
 
-    private
-
+  private
   def customer_params
     params.require(:customer).permit(:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count)
   end
+
 end
