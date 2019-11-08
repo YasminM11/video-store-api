29431ab7758417b54fdd2c43bb0520be90adaddb
Erika <erika.maust@gmail.com>
2019-11-08 15:04:16 -0800

    Corrected rentals routes by removing hyphen, adding underscore to checkout method name. Wrote checkout controller test. Removed duplicate test helper methods.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,26 +1,33 @@
 class RentalsController < ApplicationController
+ 
+  def checkout
+    customer_id = params[:customer_id]
+    movie_id = params[:movie_id]
+    
+    rental = Rental.new(rental_params)
+    customer = Customer.find_by(id: customer_id)
+    movie = Movie.find_by(id: movie_id)
+    rental.due_date = DateTime.now + 7
+    
+    if rental.save
+    render json: rental, status: :ok
+    customer.checkout += 1
+    # movie.inventory -= 1
+    customer.save!
+    else
+      render json: { errors: ["Not Found"] }, status: :bad_request
+    end
+  end
 
-  
-
-  # def show
-  #   rental = Rental.find_by(id: params:[:id])
-  #     if rental
-  #       render json: rental.as_json(only: [:id, :customer_id, :movie_id, :checkout, :due_date])
-  #       return
-  #     else
-  #       render json: { ok: false, errors: ["Not Found"] }, status: :not_found
-  #       return
-  #     end
-  # end
-
-  # def create
-  #   rental = Rental.new(rental_params)
-  #   if rental.save
-  #     render json: rental.as_json(only: [:id]), status: :created
-  #     return
+  # def check_in
+  #   movie = Movie.find_by(rental_params[:movie_id])
+  #   movies_available = movie.available_inventory
+  #   if movie.nil?
+  #   render json: { errors: ["Not Found"] }, status: :bad_request
+  #   elsif  movies_available
+  #     render json: movie, status: :ok
   #   else
-  #     render json: {ok: false, errors: rental.errors.messages }, status: :bad_request
-  #     return
+  #     render_error
   #   end
   # end
 
